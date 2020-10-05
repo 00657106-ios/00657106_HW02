8475f4fddcd10db853f77fd436dc59db8363c984
amity871028 <amity3744039@gmail.com>
2020-10-05 13:59:38 +0800

    Initial Commit

---
+++ 00657106_HW02/_0657106_HW02App.swift
@@ -0,0 +1,17 @@
+//
+//  _0657106_HW02App.swift
+//  00657106_HW02
+//
+//  Created by User04 on 2020/10/5.
+//
+
+import SwiftUI
+
+@main
+struct _0657106_HW02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
