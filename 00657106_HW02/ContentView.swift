760bc286c34579090d6460ae8269866d7b5f6fa3
amity871028 <amity3744039@gmail.com>
2020-10-05 14:08:45 +0800

    picture with commit

--- 00657106_HW02/ContentView.swift
+++ 00657106_HW02/ContentView.swift
@@ -36,7 +36,7 @@ struct ContentView_Previews: PreviewProvider {
 
 struct Abc: View {
     var body: some View {
-        ZStack {
+        ZStack {//body color
             Path { (path) in
                 
                 path.move(to: CGPoint(x: 87, y: 136))
@@ -46,17 +46,20 @@ struct Abc: View {
                 path.addQuadCurve(to: CGPoint(x: 88, y:108), control: CGPoint(x: 99 ,y: 100))
                 path.addQuadCurve(to: CGPoint(x: 87, y:136), control: CGPoint(x: 75, y: 131))
                 path.closeSubpath()
+                //ball color
                 path.addPath(Path(ellipseIn: CGRect(x: 63, y: 120 ,width: 15,height: 15)))
                 
                 
                 
+                
             }.fill(Color(red: 0, green: 1, blue: 0))
-            
+            //link ball line
             Path { (path) in
                 path.move(to: CGPoint(x: 115, y: 72))
                 path.addQuadCurve(to: CGPoint(x: 87, y: 88), control: CGPoint(x: 80, y: 50))
                 path.addQuadCurve(to: CGPoint(x: 72, y: 121), control: CGPoint(x: 90, y: 100))
             }.stroke()
+            //body outline
             Path { (path) in
                 
                 path.move(to: CGPoint(x: 87, y: 136))
@@ -66,7 +69,9 @@ struct Abc: View {
                 path.addQuadCurve(to: CGPoint(x: 88, y:108), control: CGPoint(x: 99 ,y: 100))
                 path.addQuadCurve(to: CGPoint(x: 87, y:136), control: CGPoint(x: 75, y: 131))
                 path.closeSubpath()
+                //ball outline
                 path.addPath(Path(ellipseIn: CGRect(x: 63, y: 120 ,width: 15,height: 15)))
+                //mouth
                 path.move(to: CGPoint(x: 137, y: 123))
                 path.addQuadCurve(to: CGPoint(x: 131, y:124), control: CGPoint(x: 136, y: 130))
                 path.addQuadCurve(to: CGPoint(x: 125, y:124), control: CGPoint(x: 130, y: 130))
@@ -74,14 +79,14 @@ struct Abc: View {
             }.stroke()
             
             Path { (path) in
-                
+                //eyes_black
                 path.addPath(Path(ellipseIn: CGRect(x: 108, y: 108,width: 13,height: 13)))
                 path.addPath(Path(ellipseIn: CGRect(x: 148, y: 108,width: 13,height: 13)))
                 
             }.fill(Color(red: 0, green: 0, blue: 0))
             
             Path { (path) in
-                
+                //eyes_yellow
                 path.addPath(Path(roundedRect: CGRect(x: 108, y: 113, width: 13,
                                                       height: 2), cornerRadius: 0))
                 path.addPath(Path(roundedRect: CGRect(x: 148, y: 113, width: 13,
